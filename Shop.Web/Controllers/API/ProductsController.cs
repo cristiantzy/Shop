271f85e22c4cb2c6ed71687f1ad1f8b8ce76295a
Cristian Tisoy Hurtado <37725333+cristiantzy@users.noreply.github.com>
2019-05-25 16:51:55 -0500

    API ProductsController

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productsRepository;
+
+        public ProductsController(IProductRepository productsRepository)
+        {
+            this.productsRepository = productsRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetProducts() {
+
+            return Ok(this.productsRepository.GetAll());
+        }
+      
+    }
+}
